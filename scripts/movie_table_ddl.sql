31bb71a7663dba4b47cc0d593c470c8c86b584c9
Chris Hoina <chrishoina@gmail.com>
2024-07-18 09:46:46 -0400

    updates

---
+++ scripts/movie_table_ddl.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "MOVIE" (
+    "MOVIE_ID"     NUMBER,
+    "TITLE"        VARCHAR2(200 BYTE),
+    "BUDGET"       NUMBER,
+    "GROSS"        NUMBER,
+    "LIST_PRICE"   NUMBER,
+    "GENRES"       JSON,
+    "SKU"          VARCHAR2(30 BYTE),
+    "YEAR"         NUMBER,
+    "OPENING_DATE" DATE,
+    "VIEWS"        NUMBER,
+    "CAST"         JSON,
+    "CREW"         JSON,
+    "STUDIO"       JSON,
+    "MAIN_SUBJECT" VARCHAR2(500 BYTE),
+    "AWARDS"       JSON,
+    "NOMINATIONS"  JSON,
+    "RUNTIME"      NUMBER,
+    "SUMMARY"      CLOB
+)
+NO INMEMORY;
+
+CREATE UNIQUE INDEX "PK_MOVIE_ID" ON
+    "MOVIE" (
+        "MOVIE_ID"
+    );
+
+ALTER TABLE "MOVIE"
+    ADD CONSTRAINT "PK_MOVIE_ID" PRIMARY KEY ("MOVIE_ID")
+        USING INDEX "PK_MOVIE_ID" ENABLE;
